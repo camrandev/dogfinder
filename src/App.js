1abbbc2500b629795b52a137e953a778a10b06b3
Crynowecer0 <crynowecer11@gmail.com>
2023-05-29 17:16:36 -0400

    progress on getting dogs from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [dogs, setDogs] = useState(null);
+
+  async function getDogs() {
+    const response = await fetch("http://localhost:5001/dogs");
+    const data = await response.json();
+    setDogs(data);
+  }
+
+  if (!dogs) {
+    getDogs();
+    return <h1>Loading....</h1>;
+  }
+
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <h1>hi</h1>
+      {/* <Nav/>
+      <Routes/> */}
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+ /** DESCRIPTION
+*
+* Props:
+*
+* State:
+*
+* PARENT -> Nav -> {CHILDREN}
+*/
+
+function Nav ({dogNames}) {
+
+  return (
+    <div>
+      <h3>Nav</h3>
+    </div>
+  )
+}
+
+export default Nav;
